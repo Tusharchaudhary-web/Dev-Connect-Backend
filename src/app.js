d04bda673ff8076402a054a75e684bfb92f22127
Tushar Chaudhary <tusharchaudhary12dec@gmail.com>
2025-09-23 01:16:30 +0530

    netlify link

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ app.use(cookieParser());
 
 
 app.use(cors({ 
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://cerulean-paprenjak-d522f4.netlify.app/",
     credentials:true   // The backend should know where your frontend is hosted , whitelisting the domain 
 }));
 
